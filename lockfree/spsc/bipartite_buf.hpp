7be24c4ca4eaba0e620b1c7d754eed3367647e36
Djordje Nedic <nedic.djordje2@gmail.com>
2023-05-27 17:31:02 +0200

    Release 2.0.0

--- lockfree/spsc/bipartite_buf.hpp
+++ lockfree/spsc/bipartite_buf.hpp
@@ -4,8 +4,8 @@
  * standard c++11 suitable for all systems, from low-end
  * microcontrollers to HPC machines.
  * Lock-free for single consumer single producer scenarios.
- * @version	1.2.0
- * @date 19. May 2023
+ * @version	2.0.0
+ * @date 27. May 2023
  * @author Djordje Nedic
  **************************************************************/
 
@@ -37,7 +37,7 @@
  * This file is part of lockfree
  *
  * Author:          Djordje Nedic <nedic.djordje2@gmail.com>
- * Version:         v1.1.0
+ * Version:         v2.0.0
  **************************************************************/
 
 /************************** INCLUDE ***************************/
