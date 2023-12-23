f938af6cf29dfebb9883b9c9e780a82643a8426e
Pablo Duboue <pablo.duboue@gmail.com>
2023-12-23 10:41:50 -0800

    Priority Queue Multithreaded Test

--- tests/spsc/priority_queue.cpp
+++ tests/spsc/priority_queue.cpp
@@ -1,5 +1,6 @@
 #include <algorithm>
 #include <math.h>
+#include <thread>
 
 #include <catch2/catch_test_macros.hpp>
 
@@ -64,6 +65,80 @@ TEST_CASE("Write multiple with different priority and read back ensuring "
     pop_success = queue.Pop(read);
     REQUIRE(pop_success);
     REQUIRE(read == 1024);
+}
+
+TEST_CASE("Multithreaded read/write", "[pq_multithreaded]") {
+    lockfree::spsc::PriorityQueue<uint64_t, 10, 4> queue;
+    std::vector<std::thread> threads;
+    std::vector<uint64_t> written;
+    std::vector<uint64_t> read;
+
+    // consumer
+    threads.emplace_back([&]() {
+        uint64_t value = 0;
+        uint64_t cnt = 0;
+        do {
+            bool pop_success = queue.Pop(value);
+            if (pop_success) {
+              read.push_back(value);
+              cnt++;
+            }
+        } while (cnt < TEST_MT_TRANSFER_CNT);
+    });
+
+    // producer
+    threads.emplace_back([&]() {
+        uint64_t cnt = 0;
+        uint64_t value = 0;
+        uint8_t prio = 0;
+        do {
+          value = cnt << 2 + prio;
+          bool push_success = queue.Push(value, prio);
+          if (push_success) {
+            written.push_back(value);
+            prio = (prio + 1) % 4;
+            cnt++;
+          }
+        } while (cnt < TEST_MT_TRANSFER_CNT + 1);
+    });
+    for (auto &t : threads) {
+        t.join();
+    }
+    // check at all times no higher value is present in the `written` vector
+    std::vector<bool> consumed(written.size(), false);    
+    uint64_t value1, value2;
+    uint8_t prio1, prio2;
+    size_t start = 0;
+    bool all_consumed;
+    for(size_t idx=0; idx<read.size(); idx++) {
+      // the value was read
+      value1 = read[idx];
+      prio1 = value1 & ((1<<2) - 1);
+
+      all_consumed = true;
+      for(size_t idx2=start; idx2<written.size(); idx2++) {
+        if(consumed[idx2]) {
+          if(all_consumed){
+            start++;
+          }
+          continue;
+        }
+        all_consumed = false;
+        // find when the value was written
+        if(written[idx2] == value1) {
+            consumed[idx2] = true;
+            for(size_t idx3=start; idx3 < idx2; idx3++) {
+              if(consumed[idx3]) {
+                continue;
+              }
+              value2 = written[idx3];
+              prio2 = value2 & ((1<<2) - 1);
+              REQUIRE(prio2 <= prio1);
+            }
+            break;
+        }
+      }
+    }
 }
 
 TEST_CASE("Optional API", "[pq_optional_api]") {
